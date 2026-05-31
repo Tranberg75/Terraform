7209fec832ff79c795ec63c9ba388eab2e90acd7
Tranberg <privat@ttgroup.dk>
2026-05-31 14:37:18 +0200

    ny main.tf opdatering

--- main.tf
+++ main.tf
@@ -1,28 +1,46 @@
 resource "proxmox_vm_qemu" "vm-instance" {
-    name                = "vm-instance"
-    target_node         = "odin"
-    clone               = "Ubuntu-2604-template"
-    full_clone          = true
-    cpu {
+    name = "vm-instance"
+    target_node = "odin"
+    vmid = 999
+    description = "This is a test VM created using Terraform and Proxmox provider"
+    bios = "ovmf"
+    start_at_node_boot = false
+    agent = 1
+    clone = "Ubuntu-2604-template"
+    full_clone = true
+    os_type = "ubuntu"
+    memory = 4096
+    skip_ipv6 = true
+
+cpu{
         cores = 2
-    }
-        memory              = 2048
-        bios = "ovmf"
+        sockets = 1
+        type = "x86-64-v2-AES"
 
-    disk {
-        slot = "scsi0"
-        size            = "32G"
-        type            = "disk"
-        storage         = "local-lvm"
     }
 
-    network {
+vga{
+        type = "serial0"
+    }
+
+network {
         id = 0
         model = "virtio"
         bridge = "vmbr1"
-        VLAN = 80
+        tag = 80
         firewall  = false
         link_down = false
     }
+
+disk {
+        id = 0
+        type = "scsi"
+        storage = "local-lvm"
+        storage_type = "lvm"
+        size = "20G"
+        discard = "on"   
+        slot = "scsi0"
+        
+    }    
 
 }
